84d5c67947754aeeb97cd46730ef57a720ae105b
sebastkm <s.mitusch@hotmail.com>
2017-05-10 11:18:51 +0200

    scripts: add checks for google analytics tracking
    Some activities were not properly tracked. This test should detect that before
    any new release.
    
    Related-to: #106 (NewsDetailActivity does not seem to have the google analytics tracker enabled.)

---
+++ scripts/easytracker-check.sh
@@ -0,0 +1,18 @@
+!#/bin/bash
+
+for activity in app/src/main/java/com/profsys/*/activity/*.java
+do
+if grep "EasyTracker.getInstance(this).activityStart(this);" $activity; then
+echo OK
+else
+echo $activity MISSING EASYTRACKING IN ACTIVITYSTART
+fi
+
+
+if grep "EasyTracker.getInstance(this).activityStop(this);" $activity; then
+echo OK
+else
+echo $activity MISSING EASYTRACKING IN ACTIVITYSTOP
+fi
+
+done
